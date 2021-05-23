4483b00416084053a07b79363843eae68d747d33
Kaue Massucci <kaue.massucci@dasa.com>
2021-05-23 01:07:17 -0300

    test: creatte getKilledPlayter unit test

---
+++ test/getKilledPlayer.test.js
@@ -0,0 +1,17 @@
+const getKilledPlayer = require('../utils/getKilledPlayer');
+
+const PLAYER_MAL = '<world> killed Mal by MOD_TRIGGER_HURT';
+const PLAYER_ZEH = '<world> killed Zeh by MOD_TRIGGER_HURT';
+const PLAYER_ISGALAMIDO = 'Zeh killed Isgalamido by MOD_TRIGGER_HURT';
+
+test('Get the player Mal', () => {   
+  expect(getKilledPlayer(PLAYER_MAL)).toBe("Mal");  
+});
+
+test('Get the player Zeh', () => {   
+    expect(getKilledPlayer(PLAYER_ZEH)).toBe("Zeh");  
+});
+
+test('Get the player Isgalamido', () => {   
+    expect(getKilledPlayer(PLAYER_ISGALAMIDO)).toBe("Isgalamido");  
+});
