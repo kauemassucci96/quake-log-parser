4dd88ed8b69faf9bf447ce86a96f1c17024ec4fa
Kaue Massucci <kaue.massucci@dasa.com>
2021-05-24 18:29:36 -0300

    fix: corrections in unit tests

--- test/addPlayers.test.js
+++ test/addPlayers.test.js
@@ -4,41 +4,44 @@ const initializeInfo = require('../utils/initializeInfo');
 const ZEH_PLAYER = "Zeh";
 const WORLD_PLAYER = "<world>"
 
-let gameJSON = {
+let gameInfo = {
 	game: {		
 		players: [],
 		total_kills: 0,		
 		kills: {},
 		kills_by_means: {},
+		ranking: {}		
 	}
 }
 
-gameJSON = initializeInfo(gameJSON);
+gameInfo = initializeInfo(gameInfo);
 
-let mock_1 = {
-	"game": {},
-	"kills": {},
-	"kills_by_means": {},
-	"players": [],
-	"total_kills": 0,
+let mock_1 = {	
+	kills: {},
+	kills_by_means: {},
+	players: [],
+	total_kills: 0,	
+	ranking: {}	
 }
 
-let mock_2 = {
-	"game": {},
-	"kills": { "Zeh": 0},
-	"kills_by_means": {},
-	"players": [ "Zeh" ],
-	"total_kills": 0,
+let mock_2 = {	
+	kills: {
+		'Zeh': 0
+	},
+	kills_by_means: {},
+	players: [ 'Zeh'],
+	total_kills: 0,	
+	ranking: {}	
 }
-	
+
 test('Not add <world> in players array', () => {   
-  expect(addPlayers(WORLD_PLAYER, gameJSON)).toEqual(mock_1);  
+  expect(addPlayers(WORLD_PLAYER, gameInfo)).toEqual(mock_1);  
 });
 
 test('Add Zeh in players array', () => {   
-  expect(addPlayers(ZEH_PLAYER, gameJSON)).toEqual(mock_2);  
+  expect(addPlayers(ZEH_PLAYER, gameInfo)).toEqual(mock_2);  
 });
 
 test('Not add zeh in players array again', () => {   
-  expect(addPlayers(ZEH_PLAYER, gameJSON)).toEqual(mock_2);  
+  expect(addPlayers(ZEH_PLAYER, gameInfo)).toEqual(mock_2);  
 });
