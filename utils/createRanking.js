2352257e09a58a996e2b022d1ed5e372b0182003
Kaue Massucci <kaue.massucci@dasa.com>
2021-05-24 18:44:32 -0300

    feat: code improvements on index and improvementst in project structure

--- utils/createRanking.js
+++ utils/createRanking.js
@@ -1,20 +1,23 @@
-//create the ranking by kills
-function createRanking(gameJSON) {     
+// Ceate the ranking by kills
+function createRanking(gameInfo) {     
     let orderKills = [];
     
-    for(var players in gameJSON.kills) {    
-        orderKills.push([players, gameJSON.kills[players]]);
+    // Adds each player to the array of kills in the array
+    for(var players in gameInfo.kills) {    
+        orderKills.push([players, gameInfo.kills[players]]);
     }
 
+    // Change the player position if he has a higher kills score
     orderKills.sort(function(a, b) {
         return b[1] - a[1];
     })    
 
     let ranking = {};
+    // Adds only the player and the score to the ranking variable
     orderKills.forEach(function(item){
         ranking[item[0]]=item[1];
     });
-        
+    
     return ranking;
 }
 
